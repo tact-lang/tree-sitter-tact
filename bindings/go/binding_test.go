09c57b6b9759560b4d067e0546c9953ee0e065da
Novus Nota <68142933+novusnota@users.noreply.github.com>
2024-09-29 18:53:41 +0200

    fix: bugfix release 1.5.1 (#41)
    * chore: upgrade tree-sitter-cli to 0.23.0
    
    * fix: resolve parsing problems with asm functions, that caused the tree-sitter to break and halt
    
    Returned the parsing speed to normal (i.e. super-duper fast)
    
    * chore: bump version to 1.5.1
    
    * fix: silence broken Rust tests
    
    * chore: bump the Rust binding to their most latest upstream sources

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,8 +3,8 @@ package tree_sitter_tact_test
 import (
 	"testing"
 
-	tree_sitter "github.com/smacker/go-tree-sitter"
-	"github.com/tree-sitter/tree-sitter-tact"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+	tree_sitter_tact "github.com/tree-sitter/tree-sitter-tact/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
